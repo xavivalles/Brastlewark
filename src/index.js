006bbf28f2a76cfaf8a14a2cfb083211e85c67a7
Xavier Valles <xavier.vallesvicedo@travelport.com>
2018-10-30 21:46:33 +0100

    removed not used element

--- src/index.js
+++ src/index.js
@@ -21,10 +21,7 @@ const persistConfig = {
 
 const pReducer = persistReducer(persistConfig, reducer);
 
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
-
 
 const sagaMiddleware = createSagaMiddleware();
 
